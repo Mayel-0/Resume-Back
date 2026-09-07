44167550a38a135276bb4378f910b42ef50e7306
mael <llado.mael.33@gmail.com>
2026-09-07 11:10:40 +0200

    rm consol log

--- src/middleware/auth.middleware.js
+++ src/middleware/auth.middleware.js
@@ -1,8 +1,6 @@
 import jwt from "jsonwebtoken";
 
 export const verifyToken = (req, res, next) => {
-  console.log("COOKIES REÇUS:", req.cookies); // ← log temporaire
-  console.log("HEADERS:", req.headers.cookie); // ← log temporaire
   const token = req.cookies?.token;
 
   if (!token) {
