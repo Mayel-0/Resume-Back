83b9382814842a2f5d52932798517d142de4d925
mael <llado.mael.33@gmail.com>
2026-09-07 18:25:17 +0200

    order

--- src/controllers/brief.controller.js
+++ src/controllers/brief.controller.js
@@ -3,11 +3,11 @@ import { briefs } from "../db/schema.js";
 
 export const getAllBriefs = async (req, res) => {
   try {
-    const allBriefs = await db.select().from(briefs);
+    const allBriefs = await db.select().from(briefs).orderBy(briefs.order);
     res.status(200).json(allBriefs);
   } catch (error) {
     res
       .status(500)
-      .json({ error: "Erreur lors de la récupération des projets" });
+      .json({ error: "Erreur lors de la récupération des briefs" });
   }
 };
